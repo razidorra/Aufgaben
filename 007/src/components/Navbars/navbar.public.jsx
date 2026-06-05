99474488c82862e61df85022fd19712f8403c05c
Razi <dorra.razi@gmail.com>
2026-06-05 12:49:49 +0200

    Changes made

--- 007/src/components/Navbars/navbar.public.jsx
+++ 007/src/components/Navbars/navbar.public.jsx
@@ -43,7 +43,7 @@ export function NavbarPublic() {
             </li>
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <a className="btn btn-ghost text-xl">PokeMon</a>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
